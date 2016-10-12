5c105f71d4f4c0642a9943fdb40df101fdd9e913
Aaron Kuo <atk.cloud1985xp@gmail.com>
2016-10-13 01:53:43 +0800

    feat(Order): Edit Order Basically
    - 先實作基本的編輯，只能修改 order 的欄位

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,4 +6,23 @@ class Admin::OrdersController < Admin::BaseController
   def show
     @order = Order.find(params[:id])
   end
+
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update_attributes(order_params)
+      redirect_to admin_order_path(@order), notice: 'Updated Successfully.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :email, :mobile, :paid)
+  end
 end
