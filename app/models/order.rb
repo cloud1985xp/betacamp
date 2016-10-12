f902e0573ec2d83dba0f268b78c2aafb8c62eeb7
Aaron Kuo <atk.cloud1985xp@gmail.com>
2016-10-13 03:09:10 +0800

    feat(Order): Add Callback to Save Changed Data
    - 利用 Model 的 callback 來完成
    - 當訂單被 save 時，將 dirty object 提供的 changes 資料，儲到 change records 裡

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,8 +5,16 @@ class Order < ApplicationRecord
   validates_presence_of :name, :email, :amount
   accepts_nested_attributes_for :order_items, allow_destroy: true, reject_if: :all_blank
 
+  after_save :record_changes
+
   def calculate_amount
     self.amount = order_items.sum(&:subtotal)
     self.save!
+  end
+
+  private
+
+  def record_changes
+    change_records.create(values: changes) if changed?
   end
 end
