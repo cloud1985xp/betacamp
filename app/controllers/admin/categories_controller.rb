b48f1ad149c2494dbffa1bb63e562ab414ea08ef
Aaron Kuo <atk.cloud1985xp@gmail.com>
2016-10-12 22:55:00 +0800

    feat(Admin): CRUD for Categories
    - 實作後台 Categories 的 CRUD
    - 注意表單 form_for 傳入的第一個參數，需以陣列的方式傳入 namespace 與表單處理的對象(:admin, @category)

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,54 @@
+class Admin::CategoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin!
+
+  def index
+    @categories = Category.page(params[:page])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admin_categories_path, notice: 'Created Successfully.'
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      redirect_to admin_categories_path, notice: 'Updated Successfully.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to admin_categories_path, notice: 'Category was deleted successfully'
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :description, :enabled)
+  end
+
+  def authenticate_admin!
+    redirect_to root_path, notice: 'you are not admin' unless current_user.admin?
+  end
+end
