66b350b775d7c2d7b2db19e3cb8862980d5e19bb
Aaron Kuo <atk.cloud1985xp@gmail.com>
2016-10-13 00:33:29 +0800

    feat(Model): Change Definition of Product.active
    - 更改 active scope 的定義

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
   belongs_to :category
   validates_presence_of :name, :price
 
-  scope :active, ->{ where(active: true) }
+  scope :active, ->{ where(active: true).where('shelved_on <= ?', Date.current) }
 end
