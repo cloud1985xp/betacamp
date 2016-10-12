0529c31b3cc9730b3a1e23a74c668ee21f90cf53
Aaron Kuo <atk.cloud1985xp@gmail.com>
2016-10-13 00:58:38 +0800

    feat(Cart): Show Cart
    - 實作瀏覽購物車
    - 目前只有空的情況，先把 view 檔案加上
    - 這裡我們先直接只用 session id 來紀錄 cart，這樣可以讓未登入的使用者也可以使用購物車

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,8 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_items, dependent: :destroy
   has_many :items, through: :cart_items
+
+  def empty?
+    cart_items.empty?
+  end
 end
