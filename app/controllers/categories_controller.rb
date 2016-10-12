ca844a5d3c6d1b8740f314c9ada63530d6922488
Aaron Kuo <atk.cloud1985xp@gmail.com>
2016-10-13 02:39:20 +0800

    feat(Model): Add #includes for querying collection
    - 利用 includes 來避免 n+1 的情況發生

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,6 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.enabled.find(params[:id])
     @products = @category.products.active.page(params[:page])
+    @comments = @category.comments.includes(:user)
   end
 end
