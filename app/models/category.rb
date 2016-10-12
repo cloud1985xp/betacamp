cf60f6b1e249eec2bd51847839954b225faa2760
Aaron Kuo <atk.cloud1985xp@gmail.com>
2016-10-13 01:26:12 +0800

    feat(Model): Add Self Reference to Category
    - 注意 Category#parent 與 Category#children 的 association 定義

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,9 @@
 class Category < ApplicationRecord
   has_many :products, dependent: :destroy
+  has_many :children, class_name: 'Category', foreign_key: 'parent_id', dependent: :destroy
+  belongs_to :parent, class_name: 'Category'
   validates_presence_of :name
-  acts_as_list
+  acts_as_list scope: :parent
 
   scope :enabled, ->{ where(enabled: true) }
 end
