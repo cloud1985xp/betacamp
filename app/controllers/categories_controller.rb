60bf8d73236d78e5999dc5d0137c77cc1255a61f
Aaron Kuo <atk.cloud1985xp@gmail.com>
2016-10-13 00:28:11 +0800

    feat(Scope): Replace query with scope in controllers
    - 將 controller 裡原本直接寫的 where 條件改用定義好的 scope 取代
    - DRY
    - 未來若要調整 Product 的 active 定義時，直接修改 scope 即可

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def show
-    @category = Category.find(params[:id])
-    @products = @category.products.page(params[:page])
+    @category = Category.enabled.find(params[:id])
+    @products = @category.products.active.page(params[:page])
   end
 end
--- app/controllers/category_products_controller.rb
+++ app/controllers/category_products_controller.rb
@@ -2,7 +2,7 @@ class CategoryProductsController < ApplicationController
   before_action :find_category
 
   def show
-    @product = @category.products.where(active: true).find(params[:id])
+    @product = @category.products.active.find(params[:id])
   end
 
   private
@@ -10,6 +10,6 @@ class CategoryProductsController < ApplicationController
   # Check it in`rake routes`
   # category_product GET /categories/:category_id/products/:id(.:format) category_products#show
   def find_category
-    @category = Category.where(enabled: true).find(params[:category_id])
+    @category = Category.enabled.find(params[:category_id])
   end
 end
